3f571aa40b47469f1a6e38f60651a30d2df07263
AlesciDavide <davi.alesci@email.com>
2024-05-08 14:47:30 +0200

    aggiunta controllo valori inseriti (età e km da percorrere)

--- js/script.js
+++ js/script.js
@@ -4,13 +4,14 @@ let utenteKmDaPercorrere = parseInt(prompt("Inserisci i chilomentri che desideri
 
 let prezzoBiglietto = (utenteKmDaPercorrere * 0.276);
 
+if (isNaN(utenteKmDaPercorrere) || isNaN(etaUtente) || utenteKmDaPercorrere <= 0 || etaUtente <= 0 || etaUtente > 150) {
+    alert("Attento hai inserito valori non validi");
+    location.reload();
 
-
-if (etaUtente > 65){
+}else if (etaUtente > 65){
     prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.21));
     console.log("il prezzo del tuo biglietto avendo un'età maggiore di 65 anni è:" + " " + (prezzoBiglietto.toFixed(2)));
     document.getElementById("output").innerHTML = ("il prezzo del tuo biglietto avendo un'età maggiore di 65 anni è:" + " " + (prezzoBiglietto.toFixed(2)));
-
 } else if(etaUtente < 18){
     prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.42));
     console.log("il prezzo del tuo biglietto essendo minorenne è di:" + " " + (prezzoBiglietto.toFixed(2)));
@@ -19,4 +20,5 @@ if (etaUtente > 65){
 console.log("Il prezzo del tuo biglietto non rientrando nell'età per un possibile sconto è:" + " " + (prezzoBiglietto.toFixed(2)));
 document.getElementById("output").innerHTML = ("Il prezzo del tuo biglietto non rientrando nell'età per un possibile sconto è:" + " " + (prezzoBiglietto.toFixed(2)));
 }
+
 
