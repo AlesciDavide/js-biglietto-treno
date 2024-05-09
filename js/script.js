5d0afd0f654d7cf44a3c39d45acc76cbc129a23b
AlesciDavide <davi.alesci@email.com>
2024-05-09 10:27:27 +0200

    modifica sconti - aggiunta Number. ai parsInt

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-let etaUtente = parseInt(prompt("Inserisci la tua età"));
+let etaUtente = Number.parseInt(prompt("Inserisci la tua età"), 10);
 
-let utenteKmDaPercorrere = parseInt(prompt("Inserisci i chilomentri che desideri percorrere per calcolare l'esatto importo del tuo biglietto"));
+let utenteKmDaPercorrere = Number.parseFloat(prompt("Inserisci i chilomentri che desideri percorrere per calcolare l'esatto importo del tuo biglietto"), 10);
 
 let prezzoBiglietto = (utenteKmDaPercorrere * 0.276);
 
 if (isNaN(utenteKmDaPercorrere) || isNaN(etaUtente) || utenteKmDaPercorrere <= 0 || etaUtente <= 0 || etaUtente > 150) {
     alert("Attento hai inserito valori non validi");
-    location.reload();
+    location.reload(etaUtente);
 
 }else if (etaUtente > 65){
-    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.21));
+    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.42));
     console.log("il prezzo del tuo biglietto avendo un'età maggiore di 65 anni è:" + " " + (prezzoBiglietto.toFixed(2)));
     document.getElementById("output").innerHTML = ("il prezzo del tuo biglietto avendo un'età maggiore di 65 anni è:" + " " + (prezzoBiglietto.toFixed(2)));
 } else if(etaUtente < 18){
-    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.42));
+    prezzoBiglietto = (prezzoBiglietto - (prezzoBiglietto * 0.21));
     console.log("il prezzo del tuo biglietto essendo minorenne è di:" + " " + (prezzoBiglietto.toFixed(2)));
     document.getElementById("output").innerHTML = ("il prezzo del tuo biglietto essendo minorenne è di:" + " " + (prezzoBiglietto.toFixed(2)));
 }else{
